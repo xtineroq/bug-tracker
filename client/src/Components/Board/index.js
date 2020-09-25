1d314b1f9535f47ddb25c05e8b9a1b48c6cf06ee
xtineroq <mcroque89@gmail.com>
2020-09-26 01:47:14 +1000

    separate login-sign up form

---
+++ client/src/Components/Board/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+function Board() {
+    return (
+        <Container maxWidth="lg" className="main">
+
+        </Container>
+    );
+}
+
+export default Board;
--- client/src/Components/LogIn/index.js
+++ client/src/Components/LogIn/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Form from '../Form';
 import './style.css';
 
 function Copyright() {
@@ -40,58 +37,10 @@ export default function LogIn() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <form className="form" noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className="submit"
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}>
+          <Form />
+          <Box mt={5}>
               <Copyright />
-            </Box>
-          </form>
+          </Box>
         </div>
       </Grid>
     </Grid>
