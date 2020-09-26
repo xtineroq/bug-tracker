b3e8067f0e400d3c47c54c93b71df3023f1d25ac
xtineroq <mcroque89@gmail.com>
2020-09-26 15:45:41 +1000

    appbar and styling update

--- client/src/Components/Board/index.js
+++ client/src/Components/Board/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import NavBar from '../NavBar';
 import Footer from '../Footer';
+import './style.css';
+import Container from '@material-ui/core/Container';
 
-function Board() {
+export default function Board() {
+
     return (
         <div className="root">
             <CssBaseline />
-            <Container component="main" className="main" maxWidth="sm">
+            <Box className="nav">
+                <NavBar />
+                <Container className="main">
+                    <Box my={2}>
+                        <Button href="#" color="primary" variant="outlined" className="addBtn">
+                            + Add New Ticket
+                        </Button>
+                    </Box>
+                </Container>
                 <Footer />
-            </Container>
+            </Box>
         </div>
     );
 }
-
-export default Board;
