0d4c9d796850c6cd39c051b155ab07e74400ac1e
xtineroq <mcroque89@gmail.com>
2020-10-05 20:31:04 +1100

    fixed routing issue

--- server/src/seeders/seed.js
+++ server/src/seeders/seed.js
@@ -1,11 +1,4 @@
-let mongoose = require("mongoose");
-let db = require("../models");
-
-// connected to MongoDB Atlas
-mongoose.connect("mongodb://localhost/bugTracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+let {Bug} = require("../models");
 
 // Bug Collection Seeds
 let bugSeed = [
@@ -25,7 +18,7 @@ let bugSeed = [
   },
 ];
 
-db.Bug.deleteMany({})
+Bug.deleteMany({})
   .then(() => db.Bug.collection.insertMany(bugSeed))
   .then((data) => {
     console.log(data.result.n + " records inserted!");
