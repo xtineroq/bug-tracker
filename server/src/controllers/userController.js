680f45444b748c6de7486df15bcee2f4f1861efe
rodel <rodelrebucas@gmail.com>
2020-10-06 00:48:07 +0800

    Create user data on mongodb

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,12 +1,28 @@
 const { User } = require("../models");
+const { firebaseCreateUser } = require("../service/firebase");
 
 module.exports = {
-  save: (req, res) => {
-    User.create(req.body)
-      .then((result) => res.status(201).json(result))
-      .catch((err) => {
-        console.log(err);
-        res.status(422).json(err);
+  save: async (req, res) => {
+    try {
+      const { email, password, username } = req.body;
+      const { error } = await firebaseCreateUser({
+        email,
+        password,
+        displayName: username,
       });
+      if (error !== null) {
+        const user = await User.create(req.body);
+        /** Return only these fields, excluding password.
+         *  The password is currently saved with plain text.
+         *  For security reasons, we need to encrypt the password
+         *  to secure it.
+         */
+        const { _id, email, username, role } = user;
+        res.status(201).json({ _id, email, username, role });
+      }
+    } catch (error) {
+      // TODO: Revert creation on any error
+      res.status(422).json(error);
+    }
   },
 };
