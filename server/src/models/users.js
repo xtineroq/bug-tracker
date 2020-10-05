a619ad8674b6ed3491ed33f86aa6a216dd0f8ef6
xtineroq <mcroque89@gmail.com>
2020-10-05 23:55:48 +0800

    Create user on mongodb

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -3,32 +3,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    id: {
-        type: Number,
-        unique: true
-    },
-    email: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        required: "Email address is required"
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    username: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true
-    }
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
