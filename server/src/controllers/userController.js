e0e025e9f2512efafd9ce22fd0cbf44155218237
rodel <rodelrebucas@gmail.com>
2020-10-07 04:06:20 +0800

    Move signup on server side

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -4,25 +4,35 @@ const { firebaseCreateUser } = require("../service/firebase");
 module.exports = {
   save: async (req, res) => {
     try {
-      const { email, password, username } = req.body;
+      const { email, password, username, role } = req.body;
+      const user = await User.create(req.body);
       const { error } = await firebaseCreateUser({
         email,
         password,
-        displayName: username,
+        // Note: Firebase has specific properties for users see: https://firebase.google.com/docs/auth/web/manage-users
+        displayName: username, // So we use username as displayName
       });
-      if (error !== null) {
-        const user = await User.create(req.body);
-        /** Return only these fields, excluding password.
-         *  The password is currently saved with plain text.
-         *  For security reasons, we need to encrypt the password
-         *  to secure it.
-         */
-        const { _id, email, username, role } = user;
-        res.status(201).json({ _id, email, username, role });
+      /** On firebase error delete the saved user */
+      if (error) {
+        User.findByIdAndDelete(user._id, (err, _) => {
+          if (err !== null) res.status(400).json(err);
+        });
       }
-    } catch (error) {
-      // TODO: Revert creation on any error
-      res.status(422).json(error);
+      res.status(201).json({ id: user.id, email, username, role });
+    } catch (err) {
+      /** Create default human readable error message
+       *  We can create specific error messages to guide the user (preferred)
+       *  or just a generic error message.
+       */
+      let defaultMSG = "Invalid request.";
+      let defaultCode = null;
+
+      /** If there is an error from mongodb, extract that error message and use that value instead */
+      const { _message, code } = err;
+      if (_message) defaultMSG = _message;
+      if (code) defaultCode = code;
+
+      res.status(422).json({ message: defaultMSG, code });
     }
   },
 };
