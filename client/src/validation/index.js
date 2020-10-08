f084d01a23478f95c8d59eb03cd199771fa071a2
rodel <rodelrebucas@gmail.com>
2020-10-08 21:18:29 +0800

    separate validation schema for login and signup

--- client/src/validation/index.js
+++ client/src/validation/index.js
@@ -1,22 +1,21 @@
 import { string, object } from "yup";
 
-const VALIDATE_SIGNUP = "VALIDATE_SIGNUP";
-
-const userSchema = object().shape({
+const userLoginSchema = object().shape({
   email: string()
-    .required("Email is required")
-    .email("Email must be a valid email"),
-  password: string()
-    .required("Password is required")
-    .when("$method", (method, schema) => {
-      return method === VALIDATE_SIGNUP
-        ? schema.min(8, "Password should be at least 8 characters.")
-        : schema;
-    }),
-  username: string()
-    .required("Username is required"),
-  role: string()
-    .required("Please select a role from the list."),
+    .required("Email is required.")
+    .email("Email must be a valid email."),
+  password: string().required("Password is required."),
 });
 
-export { userSchema, VALIDATE_SIGNUP };
+const userSignUpSchema = object().shape({
+  email: string()
+    .required("Email is required.")
+    .email("Email must be a valid email."),
+  password: string()
+    .required("Password is required.")
+    .min(8, "Password should be at least 8 characters."),
+  username: string().required("Username is required."),
+  role: string().required("Please select a role from the list."),
+});
+
+export { userLoginSchema, userSignUpSchema };
