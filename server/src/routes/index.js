edb7f92edaada2f0493c8976b1f0fac61ac8fef3
xtineroq <mcroque89@gmail.com>
2020-10-09 00:25:20 +0800

    find all username

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const bugsController = require("../controllers/bugsController");
 const userController = require("../controllers/userController");
 
-router.route("/user").post(userController.save);
+router.route("/user").get(userController.findByEmail).post(userController.save);
 
-router
-  .route("/user")
-  .get(userController.findByEmail)
+router.route("/users").get(userController.findAll);
 
 router.route("/bugs").get(bugsController.findAll).post(bugsController.save);
 
