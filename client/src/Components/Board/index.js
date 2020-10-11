4f8c42f51b3b7e1fb14136c00dfa4096c0c9cb4b
xtineroq <mcroque89@gmail.com>
2020-10-12 02:46:45 +1100

    wip-horizontal scroll

--- client/src/Components/Board/index.js
+++ client/src/Components/Board/index.js
@@ -5,86 +5,97 @@ import NavBar from "../NavBar";
 import Footer from "../Footer";
 import BugCard from "../BugCard";
 import "./style.css";
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import BugForm from '../BugForm';
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import BugForm from "../BugForm";
 import Typography from "@material-ui/core/Typography";
 
 export default function Board() {
-
   return (
-    <div className="root">
+    <div className="board-root">
       <CssBaseline />
-      <Box className="nav">
-        <NavBar />
-        <div className="main">
-          <Box my={2}>
-            <BugForm
-              className="addBtn"
-            >
-              New Bug
-            </BugForm>
-          </Box>
-          <Box className="panel-container">
-            <Grid
-              container
-              spacing={1}
-            >
-              <Grid item xs>
-                <Paper className="panel">
-                  <Typography className="panel-title" style={{ fontSize: "14px" }}>
-                    BACKLOG
-                  </Typography>
-                  <BugCard />
-                </Paper>
-              </Grid>
-              <Grid item xs>
-                <Paper className="panel">
-                  <Typography className="panel-title" style={{ fontSize: "14px" }}>
-                    TO DO
-                  </Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs>
-                <Paper className="panel">
-                  <Typography className="panel-title" style={{ fontSize: "14px" }}>
-                    IN PROGRESS
-                  </Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs>
-                <Paper className="panel">
-                  <Typography className="panel-title" style={{ fontSize: "14px" }}>
-                    ON STAGING
-                  </Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs>
-                <Paper className="panel">
-                  <Typography className="panel-title" style={{ fontSize: "14px" }}>
-                    READY FOR UAT
-                  </Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs>
-                <Paper className="panel">
-                  <Typography className="panel-title" style={{ fontSize: "14px" }}>
-                    UAT APPROVED
-                  </Typography>
-                </Paper>
-              </Grid>
-              <Grid item xs>
-                <Paper className="panel">
-                  <Typography className="panel-title" style={{ fontSize: "14px" }}>
-                    LIVE ON PRODUCTION
-                  </Typography>
-                </Paper>
-              </Grid>
+      <NavBar />
+      <div className="board-main">
+        <Box my={2}>
+          <BugForm className="addBtn">New Bug</BugForm>
+        </Box>
+        {/* <Box> */}
+          <Grid spacing={1} className="panel-container">
+            <Grid item xs>
+              <Paper className="panel">
+                <Typography
+                  className="panel-title"
+                  style={{ fontSize: "14px" }}
+                >
+                  BACKLOG
+                </Typography>
+                <BugCard />
+              </Paper>
             </Grid>
-          </Box>
-        </div>
-        <Footer />
-      </Box>
+            <Grid item xs>
+              <Paper className="panel">
+                <Typography
+                  className="panel-title"
+                  style={{ fontSize: "14px" }}
+                >
+                  TO DO
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className="panel">
+                <Typography
+                  className="panel-title"
+                  style={{ fontSize: "14px" }}
+                >
+                  IN PROGRESS
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className="panel">
+                <Typography
+                  className="panel-title"
+                  style={{ fontSize: "14px" }}
+                >
+                  ON STAGING
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className="panel">
+                <Typography
+                  className="panel-title"
+                  style={{ fontSize: "14px" }}
+                >
+                  READY FOR UAT
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className="panel">
+                <Typography
+                  className="panel-title"
+                  style={{ fontSize: "14px" }}
+                >
+                  UAT APPROVED
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className="panel">
+                <Typography
+                  className="panel-title"
+                  style={{ fontSize: "14px" }}
+                >
+                  LIVE ON PRODUCTION
+                </Typography>
+              </Paper>
+            </Grid>
+          </Grid>
+        {/* </Box> */}
+      </div>
+      <Footer />
     </div>
   );
 }
